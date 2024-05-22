9659d1b10a842c47d5d56d63e0fc72bf68e83e89
Pablo Dos Santos <dossantosimas@gmail.com>
2024-05-22 12:18:36 -0500

    correccion de comando de run docker compose

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -1,5 +1,5 @@
 export const Commands = {
     linux: {
-        run_compose_servicio: (servicio: string) => `sudo docker compose -f /home/ibisa/Escritorio/installer3/docker/docker-compose.yaml -d ${servicio}`
+        run_compose_servicio: (servicio: string) => `sudo docker compose -f /home/ibisa/Escritorio/installer3/docker/docker-compose.yaml up -d ${servicio}`
     }
 }

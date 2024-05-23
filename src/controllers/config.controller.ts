fb10c18b758435aae407a7582011ee3912598daa
Pablo Dos Santos <dossantosimas@gmail.com>
2024-05-23 09:11:22 -0500

    sin log

--- src/controllers/config.controller.ts
+++ src/controllers/config.controller.ts
@@ -12,36 +12,36 @@ export async function InstallComponents(req: Request, res: Response) {
 
     //obtener contenedor influxdb
     const cont_influxdb = await DockerAPI.GetContenedor('influxdb');
-    // if (!cont_influxdb)
-    //   res.status(500).json({ msg: 'No se encontro contenedor influxdb' });
-    // else {
-    //   //body para exec
-    //   const exec_body = {
-    //     AttachStdin: false,
-    //     AttachStdout: true,
-    //     AttachStderr: true,
-    //     Cmd: ['influx', 'auth', 'list'],
-    //     Tty: false,
-    //   };
+    if (!cont_influxdb)
+      res.status(500).json({ msg: 'No se encontro contenedor influxdb' });
+    else {
+      //body para exec
+      const exec_body = {
+        AttachStdin: false,
+        AttachStdout: true,
+        AttachStderr: true,
+        Cmd: ['influx', 'auth', 'list'],
+        Tty: false,
+      };
 
-    //   //obtener ID de exec
-    //   const exec_id = await DockerAPI.ExecId(cont_influxdb.Id, exec_body);
+      //obtener ID de exec
+      const exec_id = await DockerAPI.ExecId(cont_influxdb.Id, exec_body);
 
-    //   if(!exec_id){
-    //     res.status(500).json({ msg: 'No se genero ID para exec' });
-    //   } else {
-    //     //ejecutar exec con ID
-    //     const exec_start = await DockerAPI.RunExec(exec_id.Id)
+      if(!exec_id){
+        res.status(500).json({ msg: 'No se genero ID para exec' });
+      } else {
+        //ejecutar exec con ID
+        const exec_start = await DockerAPI.RunExec(exec_id.Id)
 
-    //     //obtener token de influxdb
-    //     if(exec_start){
-    //       let lines = exec_start.split('\n');
-    //       let tokenLine = lines[1];
-    //       token = tokenLine.split(/\s+/)[3];
-    //       res.json({ token: token });
-    //     }
-    //   }
-    // }
+        //obtener token de influxdb
+        if(exec_start){
+          let lines = exec_start.split('\n');
+          let tokenLine = lines[1];
+          token = tokenLine.split(/\s+/)[3];
+          res.json({ token: token });
+        }
+      }
+    }
 
     res.json({ success: true });
   } catch (error) {
--- src/services/api.docker.services.ts
+++ src/services/api.docker.services.ts
@@ -5,7 +5,7 @@ import { IExecId, IDockerContainer } from '../models/docker.model';
 class DockerAPIServices {
   async ListaContenedores(): Promise<IDockerContainer[] | null> {
     try {
-        console.log('URL:', Commands.api_docker.host + Commands.api_docker.list)
+      console.log('URL:', Commands.api_docker.host + Commands.api_docker.list);
       const response = await fetch(
         Commands.api_docker.host + Commands.api_docker.list
       );
@@ -21,7 +21,6 @@ class DockerAPIServices {
   async GetContenedor(name: string): Promise<IDockerContainer | null> {
     try {
       const lista = await this.ListaContenedores();
-      console.log('LISTA CONTENEDORES', lista)
 
       if (lista) {
         const container_influxdb = lista.find(
@@ -65,13 +64,16 @@ class DockerAPIServices {
 
   async RunExec(exec_id: string): Promise<string | null> {
     try {
-      const response = await fetch(Commands.api_docker.host + Commands.api_docker.exec_start(exec_id), {
-        method: 'POST',
-      })
-      const data = await response.text(); 
+      const response = await fetch(
+        Commands.api_docker.host + Commands.api_docker.exec_start(exec_id),
+        {
+          method: 'POST',
+        }
+      );
+      const data = await response.text();
 
-      return data
-    }catch (error) {
+      return data;
+    } catch (error) {
       console.error('RunExec:', error);
       return null;
     }

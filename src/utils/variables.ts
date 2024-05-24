fd55061b19322d47dae5f9807fee960c39b282f7
Pablo Dos Santos <dossantosimas@gmail.com>
2024-05-24 10:56:54 -0500

    permisos para telegraf env

--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -2,28 +2,29 @@ import * as dotenv from 'dotenv';
 import * as fs from 'fs';
 import * as path from 'path';
 
+import { ComandoLocales } from '../services/comandos.services';
+
 export async function updateEnvVar(
   variable: string,
   value: string,
   direccion: string
 ): Promise<boolean> {
   try {
+    console.log('ACTUALIZANDO VAR')
     // Ruta del archivo .env
     const envPath = path.join(__dirname, direccion);
 
+    await ComandoLocales.Run('sudo chmod 777 $PWD/docker/start/telegraf.env')
+
     // Carga las variables de entorno desde .env
-    dotenv.config({ path: envPath });
+    const envConfig = dotenv.parse(fs.readFileSync(envPath));
 
     // Modifica la variable de entorno
-    process.env[variable] = value;
+    envConfig[variable] = value;
 
     // Guarda las variables de entorno modificadas en .env
-    fs.writeFileSync(
-      envPath,
-      Object.entries(process.env)
-        .map(([key, val]) => `${key}=${val}`)
-        .join('\n')
-    );
+    const envConfigString = Object.entries(envConfig).map(([key, val]) => `${key}=${val}`).join('\n');
+    fs.writeFileSync(envPath, envConfigString);
 
     return true;
   } catch (error) {

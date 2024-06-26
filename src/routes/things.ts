3e0960ede2ba1d68d5d3db843da888153f604d28
dossantosimas@gmail.com <dossantosimas@gmail.com>
2024-06-26 09:24:22 -0500

    test

--- src/routes/things.ts
+++ src/routes/things.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-import { getAllMeasurements, getAllFields } from '../controllers/thing.controller';
+import { getAllMeasurements, getAllFields, getConfig } from '../controllers/thing.controller';
 
 const router = Router();
 
 router
 .get('/measurements/all', getAllMeasurements)
 .get('/fields/all', getAllFields)
-.post('/influxdb/config')
+.post('/influxdb/config', getConfig)
 
 
 export default router;

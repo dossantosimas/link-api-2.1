7b95e71799cd98709352fef59263e7e754ce2c26
dossantosimas@gmail.com <dossantosimas@gmail.com>
2024-07-02 07:28:13 -0500

    test

--- src/lib/sqlite.ts
+++ src/lib/sqlite.ts
@@ -23,16 +23,26 @@ console.log('Dialect:', process.env.DB_DIALECT)
 export const sequelizeEdge = new Sequelize({
   dialect: process.env.DB_DIALECT as Dialect,
   storage: './docker/storage/db_ibisa.sqlite',
-  models: [Propiedades, Eventos, Label, Label_Thing, Metric, Point, Thing]
+  // models: [Propiedades, Eventos, Label, Label_Thing, Metric, Point, Thing]
+  models: [Propiedades]
+});
+
+export const sequelizeThing = new Sequelize({
+  dialect: process.env.DB_DIALECT as Dialect,
+  storage: './docker/storage/db_thing.sqlite',
+  models: [Eventos, Label, Label_Thing, Metric, Point, Thing]
 });
 
 
 export async function start_db(): Promise<void> {
   try {
     await ComandoLocales.Run('sudo chmod +777 $PWD/docker/storage/db_ibisa.sqlite');
+    await ComandoLocales.Run('sudo chmod +777 $PWD/docker/storage/db_thing.sqlite');
     await sequelizeEdge.authenticate();
+    await sequelizeThing.authenticate();
     console.log('Conexión con la base de datos edge establecida con éxito.');
     await sequelizeEdge.sync()
+    await sequelizeThing.sync()
 
     // await sequelizeMuestras.authenticate();
     // console.log('Conexión con la base de datos muestras establecida con éxito.');
